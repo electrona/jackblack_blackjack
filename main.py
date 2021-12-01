e22d854b258b2d5f1af2d02da7ff5bad11883106
Eric Thomas <ericthomas@tuta.io>
2021-12-01 19:35:23 -0330

    fixed casing as per PEP 8 and cleanup

--- main.py
+++ main.py
@@ -4,74 +4,61 @@ import dealer
 
 import random as rnd
 
-#use tableHands[] to hold multiple playerHand[]
-#use .remove()/.pop() to remove cards from single deck
-#add ability to change A to 1 OR 11
+
+# use tableHands[] to hold multiple playerHand[]
+# use .remove()/.pop() to remove cards from single deck
+# add ability to change A to 1 OR 11
 
 
-#dealing cards (player)
-def creatingPlayerHand():
+# dealing cards (player)
+def creating_player_hand():
     suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
-    cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-    handValue = 0
-    numberOfCards = 0
-    playerHand = [handValue]
-    for numberOfCards in range(2):
+    card_numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+    hand_value = 0
+    number_of_cards = 0
+    player_hand = [hand_value]
+    for number_of_cards in range(2):
         h = rnd.randrange(13)
-        cardNumber = str(cardNumbers[h])
+        card_number = str(card_numbers[h])
         j = rnd.randrange(3)
         suit = suits[j]
-        playerHand.append(cardNumber)
-        playerHand.append(suit)
-    return playerHand
+        player_hand.append(card_number)
+        player_hand.append(suit)
+    return player_hand
 
-#takes face value and assigns a numerical value
-def cardValueCalculation(playerHand):
-    handValue = 0
-    cardValue = 0
-    for i in range(1, len(playerHand), 2):
-        cardNumber = playerHand[i]
-        if cardNumber == "J":
-            cardValue = 10
-        elif cardNumber == "Q":
-            cardValue = 10
-        elif cardNumber == "K":
-            cardValue = 10
-        elif cardNumber == "A":
-            cardValue = 11
-        elif cardNumber == "2":
-            cardValue = 2
-        elif cardNumber == "3":
-            cardValue = 3
-        elif cardNumber == "4":
-            cardValue = 4
-        elif cardNumber == "5":
-            cardValue = 5
-        elif cardNumber == "6":
-            cardValue = 6
-        elif cardNumber == "7":
-            cardValue = 7
-        elif cardNumber == "8":
-            cardValue = 8
-        elif cardNumber == "9":
-            cardValue = 9
-        elif cardNumber == "10":
-            cardValue = 10
-        handValue = handValue + cardValue
-    print("Your hand total is: " + str(handValue))
-    print()
-    playerHand[0] = handValue
-    return playerHand
 
-#the game itself
-def playGame(playerHand):
+# takes face value and assigns a numerical value
+def card_value_calculation(player_hand):
+    hand_value = 0
+    card_value = 0
+    for i in range(1, len(player_hand), 2):
+        card_number = player_hand[i]
+        if card_number == "J":
+            card_value = 10
+        elif card_number == "Q":
+            card_value = 10
+        elif card_number == "K":
+            card_value = 10
+        elif card_number == "A":
+            card_value = 11
+        else:
+            card_value = card_number
+        hand_value = hand_value + card_value
+    print("Your hand total is: " + str(hand_value))
     print()
-    #betting function/module
-    printHand(playerHand)
+    player_hand[0] = hand_value
+    return player_hand
+
+
+# the game itself
+def play_game(player_hand):
+    print()
+    # betting function/module
+    print_hand(player_hand)
     again = True
-    while again == True:
-        cardValueCalculation(playerHand)
-        handValue = playerHand[0]        
+    while again:
+        card_value_calculation(player_hand)
+        handValue = player_hand[0]
         if handValue > 21:
             print("You Bust!")
             break
@@ -80,43 +67,46 @@ def playGame(playerHand):
             break
         choice = input("Would you like to Hit or Stand (h/s) ")
         if choice.lower() == "h":
-            hit(playerHand)
+            hit(player_hand)
         elif choice.lower() == "s":
             stand()
             break
         else:
             print("Invalid Selection. Please try again.")
-        printHand(playerHand)
+        print_hand(player_hand)
     print()
 
-#to "hit" for another card
-def hit(playerHand):
+
+# to "hit" for another card
+def hit(player_hand):
     print("You request another card")
     suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
-    cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+    card_numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
     h = rnd.randrange(13)
-    cardNumber = str(cardNumbers[h])
+    card_number = str(card_numbers[h])
     j = rnd.randrange(3)
     suit = suits[j]
-    playerHand.append(cardNumber)
-    playerHand.append(suit)
+    player_hand.append(card_number)
+    player_hand.append(suit)
     print()
 
-#to stay with your current hand
+
+# to stay with your current hand
 def stand():
     print("You are content with your current hand")
 
-#prints the players hand in organized manner 
-def printHand(playerHand):
-    for i in range(1, len(playerHand), 2):
-        print("[" + playerHand[i] + playerHand[i+1] + "]")
+
+# prints the players hand in organized manner
+def print_hand(player_hand):
+    for i in range(1, len(player_hand), 2):
+        print("[" + player_hand[i] + player_hand[i + 1] + "]")
     print()
-    
+
+
 def main():
-    playerHand = creatingPlayerHand()
-    playGame(playerHand)
-    
+    player_hand = creating_player_hand()
+    play_game(player_hand)
+
 
 if __name__ == "__main__":
     main()
-
