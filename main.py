2dd3c6e84c353d01592aa5af141e5b8bf8a42d28
woolridge7 <95373639+woolridge7@users.noreply.github.com>
2021-12-06 11:41:05 -0330

    Changes to run game

--- main.py
+++ main.py
@@ -25,29 +25,34 @@ def main():
     create_player_hand(game_deck, player_hand)
     print(player_hand)
     player.card_value_calculation(player_hand)  # gets hand_value of hand
-    player.print_hand(player_hand)              # prints hand in organized manner [card][card]
+    #player.print_hand(player_hand)              # prints hand in organized manner [card][card]
 
-    # print_hand(player_hand)
-    # again = True
-    # while again:
-    #     card_value_calculation(player_hand)
-    #     hand_value = player_hand[0]
-    #     if hand_value > 21:
-    #         print("You Bust!")
-    #         break
-    #     elif hand_value == 21:
-    #         print("You win!")
-    #         break
-    #     choice = input("Would you like to Hit or Stand (h/s) ")
-    #     if choice.lower() == "h":
-    #         hit(player_hand)
-    #     elif choice.lower() == "s":
-    #         stand()
-    #         break
-    #     else:
-    #         print("Invalid Selection. Please try again.")
-    #     print_hand(player_hand)
-    # print()
+    #player.print_hand(player_hand)
+    again = True
+    while again:
+         player.card_value_calculation(player_hand)
+         hand_value = player_hand[0]
+         if hand_value > 21:
+             print("You Bust!")
+             break
+         elif hand_value == 21:
+             #check to see if dealer == 21
+             #if not print
+             print("You win!")
+             #else print tie
+             break
+         choice = input("Would you like to Hit or Stand (h/s) ")
+         if choice.lower() == "h":
+             hit(game_deck, player_hand)
+             print("you hit")
+         elif choice.lower() == "s":
+             stand()
+             # show dealers hand
+             break
+         else:
+             print("Invalid Selection. Please try again.")
+    #player.print_hand(player_hand)
+    print()
 
 
 if __name__ == "__main__":
