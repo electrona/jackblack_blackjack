344dbca09a9cbd9b304eac8d99f4c6330ddc364c
DeToxFox <95373983+DeToxFox@users.noreply.github.com>
2021-12-06 18:19:35 -0330

    Worked on hit function and main plus printing errors

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import casinodeck as deck
 import casinoplayer as player
-import betting
-import dealer
+#import betting
+#import dealer
 import random as rnd
 
 
 # alternative hit method
+from jackblack_blackjack import casinoplayer
+
+
 def hit(game_deck, player_hand):
     card_given = game_deck.pop()
     player_hand.append(card_given)
+    # #casinoplayer.card_value_calculation(player_hand)
+    # #if player_hand == "A" and hand_value <= 11:
+    #         hand_value = hand_value + 10
+
 
 
 # to stay with your current hand
@@ -20,14 +27,15 @@ def create_player_hand(game_deck, player_hand):
     hit(game_deck, player_hand)
 
 def main():
+
     player_hand = [0, 0]                        # player_hand = [hand_value, chip_stack]
     game_deck = deck.new_deck()
     create_player_hand(game_deck, player_hand)
-    print(player_hand)
-    player.card_value_calculation(player_hand)  # gets hand_value of hand
+    #print(player_hand) #gets rid of the double zero's
+    #player.card_value_calculation(player_hand)  # gets hand_value of hand #this print total 2x
     player.print_hand(player_hand)              # prints hand in organized manner [card][card]
 
-    player.print_hand(player_hand)
+    #player.print_hand(player_hand) #this removed the extra hand from printing
     again = True
     while again:
          player.card_value_calculation(player_hand)
