51ccd3b6067b1941425dfe70edf3e9627f65c4bb
DeToxFox <95373983+DeToxFox@users.noreply.github.com>
2021-12-06 10:47:23 -0330

    Cleaned up commented out code, deleted redundant from first version code

--- dealer.py
+++ dealer.py
@@ -1,38 +1,25 @@
 import csv
 import casinodeck
-
-
 # import betting
 
-# FILENAME = "dealersNet.csv"
-#
-# def write_dealersNet(money): #this might have to be in the player module, assigning win/loss to csv
-#     with open(FILENAME, "w", newline="") as file:
-#         writer = csv.writer(file)
-#         writer.writerows(money)
-#
-# def read_dealerNET():
-#     money = []
-#     with open(FILENAME, newline="") as file:
-#         reader = csv.reader(file)
-#         for row in reader:
-#             money.append(row)
-#     return money
-#
-# def displayWinLoss():
-#     pass #may not need
-#
-# # def shuffle_deck(shuffled):
-# #     #import casinodeck with shuffle function
-# #     deck = casinodeck.new_deck()
-# #     return deck
-#
-# hand = []
-#     for i in range(2):
-#         #card = deck.pop()
-#     #hand.append(card)
-#     #return hand
-#     print(deck[i])
+FILENAME = "dealersNet.csv"
+
+def write_dealersNet(money): #this might have to be in the player module, assigning win/loss to csv
+    with open(FILENAME, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(money)
+
+def read_dealerNET():
+    money = []
+    with open(FILENAME, newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            money.append(row)
+    return money
+
+def displayWinLoss():
+    pass #may not need
+
 
 def dealers_hand(deck):
     hand = []
@@ -90,27 +77,27 @@ def main():
 
     deck = casinodeck.new_deck()
     dealers_hand(deck)
-    # hand = deal_cards(deck)
+    hand = dealers_hand(deck)
 
 
-# dealers_hand(hand)
+dealers_hand(hand)
 
-# current_balance = read_dealerNET()
-# #displayWinLoss() #may not need
-#
-# choice = "y"
-# while choice.lower() == "y":
-#     hand_payout = betting."futurefunction()" #hand_payout would need to be imported
-#                         #-50    +     -100   = -150
-#     net_plus_minus = (current_balance + hand_payout)
-#
-#     print("Dealers Net +/-: " + str(net_plus_minus))
-#     print()
-#
-#     # money = []
-#     # money.append(net_plus_minus)
-#
-#     choice = input("View balance again? (y/n):")
+current_balance = read_dealerNET()
+#displayWinLoss() #may not need
+
+choice = "y"
+while choice.lower() == "y":
+    hand_payout = betting."futurefunction()" #hand_payout would need to be imported
+                        #-50    +     -100   = -150
+    net_plus_minus = (current_balance + hand_payout)
+
+    print("Dealers Net +/-: " + str(net_plus_minus))
+    print()
+
+    money = []
+    money.append(net_plus_minus)
+
+    choice = input("View balance again? (y/n):")
 
 if __name__ == "__main__":
     main()
