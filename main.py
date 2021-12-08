bbff2f66846f92ab8d451889d1dc2f32dda250c4
woolridge7 <95373639+woolridge7@users.noreply.github.com>
2021-12-08 15:19:37 -0330

    couple minor fixes

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def create_player_hand(game_deck, player_hand):
 def main():
     player_chip_stacks = []
     game_deck = deck.new_deck()
+    player_float_amount = []
 
     while True:
         number_of_players = int(input("How many Players (1-5): "))  # validation between 1 & 5
@@ -35,23 +36,26 @@ def main():
     for i in range(0, number_of_players):
         chip_stacks = bet.player_float(i)
         player_chip_stacks.append(chip_stacks)
-    print(player_chip_stacks)
+        #player_float_amount.append(0)
+    # print(player_chip_stacks)  #test
 
     play_again = "y"
     while play_again.lower() == "y":
         bet_amount = []
-        dealer_hand = dealer.dealers_hand(game_deck)
-        dealer_shown_card = dealer_hand[1]
+        dealer_hand = dealer.dealers_hand(game_deck)               # not consistent
+        dealer_shown_card = dealer_hand[1]                         # stays consistent no matter the dealer hand
 
+        # round of betting before hands are dealt
         for j in range(0, number_of_players):
             player_float_amount = player_chip_stacks[j]
-            player_chip_stacks[j] = bet.betting_player(player_float_amount, j)
-            bet_made = player_chip_stacks[j]
-            player_float_amount = player_float_amount - bet_made
+            bet_made = bet.betting_player(player_float_amount, j)
+            player_chip_stacks[j] = player_float_amount - bet_made
             bet_amount.append(bet_made)
             bet_made = 0
-            print(bet_amount)
+            # print(bet_amount)               # test
+            # print(player_chip_stacks[j])    # test
             print()
+
         for i in range(0, number_of_players):
             print("===============================")
             print("Player " + str(i + 1) + "'s Turn")
@@ -59,37 +63,43 @@ def main():
             player_hand = [0, 0]
             create_player_hand(game_deck, player_hand)
             player.print_hand(player_hand)
-            print("Dealer is showing " + dealer_shown_card)
-            again = True
-            while again:
+            # print("Dealer is showing " + dealer_shown_card)
+
+            while True:
                 player.card_value_calculation(player_hand)
                 hand_value = player_hand[0]
                 if hand_value > 21:
                     print("You Bust!")
                     break
 
+                print("Dealer is showing " + dealer_shown_card)
                 choice = input("Would you like to Hit or Stand (h/s) ")
                 if choice.lower() == "h":
                     hit(game_deck, player_hand)
                     print("you hit")
                     print()
-                    player.print_hand(player_hand)
+                    # player.print_hand(player_hand)  #test
                     print("Dealer is showing " + dealer_shown_card)  # make into dealer.function()
                 elif choice.lower() == "s":
                     stand(player_hand)
-                    dealer_hand = dealer.dealers_hand(game_deck)  # show dealers hand
-                    print(dealer_hand[0])  # show dealers hand value
+                    dealer_hand = dealer.dealers_hand(game_deck)  # show dealers hand  ## changes every hand
+                    # print(dealer_hand[0])  # show dealers hand value  #test
                     print("player " + str(i + 1))
-                    print(player_chip_stacks[i])
-                    bet_made = 0
+                    # print(bet_amount)                                 #test
+                    """
                     bet_made = bet_amount[i]
                     player_hand_value = player_hand[0]
                     dealer_hand_value = dealer_hand[0]
-                    player_chip_stacks = player_chip_stacks[i]  # error on this line when dealing with player 2
+                    print(player_chip_stacks)
+                    #chip_stacks = player_chip_stacks[i]
+                    print(player_chip_stacks[i])
+                    print(chip_stacks)# error on this line when dealing with player 2
                     player_chip_stacks = bet.hand_comparison(dealer_hand_value, player_hand_value, bet_made,
-                                                             player_chip_stacks)
-                    player_float_amount = player_float_amount + player_chip_stacks
-                    print("You now have: " + str(player_float_amount) + " chips")
+                                                             chip_stacks)
+                    # player_chip_stacks[i] = (player_chip_stacks + chips_won_lost)
+                    #player_float_amount += player_chip_stacks
+                    print("You now have: " + str(chip_stacks) + " chips")
+                    """
                     break
                 else:
                     print("Invalid Selection. Please try again.")
